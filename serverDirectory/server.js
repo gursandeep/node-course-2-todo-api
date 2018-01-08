163ce54e3ab34a6d41774db059cf678706a2189b
gursandeep <gursandeep.singh@gmail.com>
2018-01-08 09:00:27 +0530

    adding new files

---
+++ serverDirectory/server.js
@@ -0,0 +1,56 @@
+var {mongoose} = require('./db/mongoose.js');
+var {Todo} = require('.models/Todo');
+var {User} = require('.models/User');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+// var newTodo = new Todo({
+//   text : "take rest",
+//   completed : true,
+//   completedAt : 6
+// });
+//
+// // newTodo.save().then((doc)=>{
+// //   console.log('Saved todo ',doc);
+// // },(e)=>{
+// //   console.log('Unable to save');
+// // });
+//
+//
+// //make user model
+// //email -require,trim,string,min lenght 1
+//
+//
+//
+// var newUser = new User({
+//   email :'abc@abc.com'
+// });
+//
+// newUser.save().then((doc)=>{
+//   console.log('Saved User ',doc);
+// },(err)=>{
+//   console.log('Error occured',err);
+// });
