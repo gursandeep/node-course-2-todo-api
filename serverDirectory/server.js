f402aa7812a070b4c2f053ee384be39ea84d4f36
gursandeep <gursandeep.singh@gmail.com>
2018-01-08 09:21:14 +0530

    add post /todos route

--- serverDirectory/server.js
+++ serverDirectory/server.js
@@ -1,9 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+
 var {mongoose} = require('./db/mongoose.js');
-var {Todo} = require('.models/Todo');
-var {User} = require('.models/User');
+var {Todo} = require('./models/Todo');
+var {User} = require('./models/User');
 
 
+var app = express();
+app.use(bodyParser.json());
 
+app.post('/todos',(req,res)=>{
+  console.log(req.body);
+  var todo = new Todo({
+    text : req.body.text
+  });
+
+  todo.save().then((doc)=>{
+    res.send(doc);
+  },(err)=>{
+    res.status(400).send(err);
+  });
+});
+
+app.listen(3000,()=>{
+  console.log('Started on port 3000');
+});
 
 
 
