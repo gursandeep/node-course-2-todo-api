8c05cbf6573160b5315714c7cedab7d23980ee42
gursandeep <gursandeep.singh@gmail.com>
2018-01-09 10:09:11 +0530

    Test post to do route

--- serverDirectory/server.js
+++ serverDirectory/server.js
@@ -3,8 +3,8 @@ const bodyParser = require('body-parser');
 
 
 var {mongoose} = require('./db/mongoose.js');
-var {Todo} = require('./models/Todo');
-var {User} = require('./models/User');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
 
 var app = express();
@@ -29,50 +29,4 @@ app.listen(3000,()=>{
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//
-// var newTodo = new Todo({
-//   text : "take rest",
-//   completed : true,
-//   completedAt : 6
-// });
-//
-// // newTodo.save().then((doc)=>{
-// //   console.log('Saved todo ',doc);
-// // },(e)=>{
-// //   console.log('Unable to save');
-// // });
-//
-//
-// //make user model
-// //email -require,trim,string,min lenght 1
-//
-//
-//
-// var newUser = new User({
-//   email :'abc@abc.com'
-// });
-//
-// newUser.save().then((doc)=>{
-//   console.log('Saved User ',doc);
-// },(err)=>{
-//   console.log('Error occured',err);
-// });
+module.exports = {app};
